477c598aac4066c237556b0f3d4efe60c05097f9
Stepan Martyushov <techseth@yandex.ru>
2024-03-21 14:57:36 +0300

    Update TriggerExample.py

--- TriggerExample.py
+++ TriggerExample.py
@@ -46,6 +46,45 @@ except:
 
     assert_pico_ok(status["ChangePowerSource"])
 
+# Set up channel A
+# handle = chandle
+channel = ps.PS5000A_CHANNEL["PS5000A_CHANNEL_A"]
+enabledA = 1
+coupling_type = ps.PS5000A_COUPLING["PS5000A_DC"]
+chARange = ps.PS5000A_RANGE["PS5000A_20V"]
+# analogue offset = 0 V
+status["setChA"] = ps.ps5000aSetChannel(chandle, channel, enabledA, coupling_type, chARange, 0)
+assert_pico_ok(status["setChA"])
+
+# Set up channel B
+# handle = chandle
+channel = ps.PS5000A_CHANNEL["PS5000A_CHANNEL_B"]
+enabledB = 0
+# coupling_type = ps.PS5000A_COUPLING["PS5000A_DC"]
+chBRange = ps.PS5000A_RANGE["PS5000A_2V"]
+# analogue offset = 0 V
+status["setChB"] = ps.ps5000aSetChannel(chandle, channel, enabledB, coupling_type, chBRange, 0)
+assert_pico_ok(status["setChB"])
+
+# Set up channel C
+# handle = chandle
+channel = ps.PS5000A_CHANNEL["PS5000A_CHANNEL_C"]
+enabledC = 0
+# coupling_type = ps.PS5000A_COUPLING["PS5000A_DC"]
+chCRange = ps.PS5000A_RANGE["PS5000A_2V"]
+# analogue offset = 0 V
+status["setChC"] = ps.ps5000aSetChannel(chandle, channel, enabledC, coupling_type, chCRange, 0)
+assert_pico_ok(status["setChC"])
+
+# Set up channel D
+# handle = chandle
+channel = ps.PS5000A_CHANNEL["PS5000A_CHANNEL_D"]
+enabledD = 0
+# coupling_type = ps.PS5000A_COUPLING["PS5000A_DC"]
+chDRange = ps.PS5000A_RANGE["PS5000A_2V"]
+# analogue offset = 0 V
+status["setChD"] = ps.ps5000aSetChannel(chandle, channel, enabledD, coupling_type, chDRange, 0)
+assert_pico_ok(status["setChD"])
 
 digital_port0 = ps.PS5000A_CHANNEL["PS5000A_DIGITAL_PORT0"]
 # Set up digital port
@@ -53,11 +92,10 @@ digital_port0 = ps.PS5000A_CHANNEL["PS5000A_DIGITAL_PORT0"]
 # channel = ps5000a_DIGITAL_PORT0 = 0x80
 # enabled = 1
 # logicLevel = 10000
-status["SetDigitalPort"] = ps.ps5000aSetDigitalPort( chandle, digital_port0, 1, 10000)
+status["SetDigitalPort"] = ps.ps5000aSetDigitalPort(chandle, digital_port0, 1, 10000)
 assert_pico_ok(status["SetDigitalPort"])
 
 #Set a trigger on digital channel
-
 
 # Set the number of sample to be collected
 preTriggerSamples = 1000
@@ -73,7 +111,7 @@ totalSamples = preTriggerSamples + postTriggerSamples
 # TimeIntervalNanoseconds = ctypes.byref(timeIntervalNs)
 # MaxSamples = ctypes.byref(returnedMaxSamples)
 # Segement index = 0
-timebase = 8
+timebase = 25 # 8
 timeIntervalNs = ctypes.c_float()
 returnedMaxSamples = ctypes.c_int16()
 status["GetTimebase"] = ps.ps5000aGetTimebase2(chandle,
@@ -105,7 +143,7 @@ status["SetDataBuffers"] = ps.ps5000aSetDataBuffers(chandle,
                                                     0)
 assert_pico_ok(status["SetDataBuffers"])
 
-# set the digital trigger for a high bit on digital channel 0
+# set the digital trigger for a high bit on digital channel 4
 conditions = ps.PS5000A_CONDITION(ps.PS5000A_CHANNEL["PS5000A_DIGITAL_PORT0"], ps.PS5000A_TRIGGER_STATE["PS5000A_CONDITION_TRUE"])
 nConditions = 1
 clear = 1
@@ -117,7 +155,7 @@ status["setTriggerChannelConditionsV2"] = ps.ps5000aSetTriggerChannelConditionsV
                                                                                   info)
 assert_pico_ok(status["setTriggerChannelConditionsV2"])
 
-directions = ps.PS5000A_DIGITAL_CHANNEL_DIRECTIONS(ps.PS5000A_DIGITAL_CHANNEL["PS5000A_DIGITAL_CHANNEL_0"], ps.PS5000A_DIGITAL_DIRECTION["PS5000A_DIGITAL_DIRECTION_HIGH"])
+directions = ps.PS5000A_DIGITAL_CHANNEL_DIRECTIONS(ps.PS5000A_DIGITAL_CHANNEL["PS5000A_DIGITAL_CHANNEL_4"], ps.PS5000A_DIGITAL_DIRECTION["PS5000A_DIGITAL_DIRECTION_HIGH"])
 nDirections = 1
 status["setTriggerDigitalPortProperties"] = ps.ps5000aSetTriggerDigitalPortProperties(chandle,
                                                                                       ctypes.byref(directions),
@@ -176,9 +214,6 @@ print ("Data collection complete.")
 
 # Obtain binary for Digital Port 0
 # The tuple returned contains the channels in order (D7, D6, D5, ... D0).
-ic(bufferDPort0Max)
-
-ic(cTotalSamples)
 bufferDPort0 = splitMSODataFast(cTotalSamples, bufferDPort0Max)
 
 # Creates the time data
@@ -201,7 +236,7 @@ print ("Plotting data...")
 
 # Plot the data from digital channels onto a graph
 
-plt.figure(num='PicoScope 3000 Series (A API) MSO Block Capture Example')
+plt.figure(num='PicoScope 5000 Series (A API) MSO Block Capture Example')
 plt.title('Plot of Digital Port 0 digital channels vs. time')
 # plt.plot(time, bufferDPort0[0], label='D7')  # D7 is the first array in the tuple.
 # plt.plot(time, bufferDPort0[1], label='D6')
