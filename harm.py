de274f36895402f48605f157ed70260f11aeb6fa
Stepan Martyushov <techseth@yandex.ru>
2024-04-09 12:10:09 +0300

    Update harm.py

--- harm.py
+++ harm.py
@@ -1,4 +1,5 @@
 from icecream import ic # Для дебага
+ic.disable
 # import time
 
 from PyQt5 import QtWidgets, QtGui
@@ -247,8 +248,9 @@ def setup_digital_channels() -> None:
 	# handle = harm.chandle
 	# channel = ps5000a_DIGITAL_PORT0 = 0x80
 	# enabled = 1
-	# logicLevel = 10000
-	harm.status["SetDigitalPort"] = ps.ps5000aSetDigitalPort(harm.chandle, digital_port0, 1, 10000)
+	# logicLevel = 10000 == 1,5 V
+	# logicLevel = 32500 == 5 V
+	harm.status["SetDigitalPort"] = ps.ps5000aSetDigitalPort(harm.chandle, digital_port0, 1, 32500)
 	assert_pico_ok(harm.status["SetDigitalPort"])
 
 def get_max_ADC_samples() -> None:
@@ -410,24 +412,28 @@ def start_record_data() -> None:
 		harm.data['ch_d'] = adc2mVChDMax
 
 	# Obtain binary for Digital Port 0
-	# The tuple returned contains the channels in order (D7, D6, D5, ... D0).
+	# The tuple returned contains the channels in order (D7, D6, D5, D4, D3, D2, D1, D0).
+	# '0' == D7, '2' == D5
 	bufferDPort0 = splitMSODataFast(cmaxSamples, bufferDPort0Max)
 	harm.data['D0'] = bufferDPort0[7]
-	harm.data['D4'] = bufferDPort0[3]
+	harm.data['D4'] = bufferDPort0[5]
 	
 	df = pd.DataFrame(harm.data)
 
 	df['D0'] = df['D0'].apply(int)
 	df['D4'] = df['D4'].apply(int)
 
+	print("Writing to file...")
+	df.to_csv("data_2024-04-09_02.csv")
+	print('Save completed')
+
 	# plot_data()
 	# print("plot ok")
-	# print("Calculating...")
+
+	print("Calculating...")
 	# calc_results(df)
 	
-	# print("Writing to file...")
-	# df.to_csv("data_2024-03-29_02.csv")
-	# print('Save completed')
+
 
 def calc_results(df: pd.core.frame.DataFrame) -> None:
 	r = 1.45 #24mm coil
@@ -438,35 +444,15 @@ def calc_results(df: pd.core.frame.DataFrame) -> None:
 	R_coilA =5*r1 + 2*h1 #radius for A coil in 30mm PCB added by PI
 	N = 2 #for quadrupole term added by PI
 	ic()
-	result = calc.integr(df)
+	result = calc.integrate_dataframe(df)
 
-	sens, Sens, Sens_m = calc.qcoef(r1, h1) #changed by PI
+	sens, Sens, Sens_m = calc.quadrupole_coefficient(r1, h1) #changed by PI
 	comp, uncomp = result
 	#comp, uncomp, sens30, Sens, R_A, N, Sens_m
-	final_result = calc.compute(comp, uncomp, sens, Sens, R_coilA, N, Sens_m)#changed by PI
+	final_result = calc.harmonic_calculation(comp, uncomp, sens, Sens, R_coilA, N, Sens_m)#changed by PI
 
-	# final_result = ([0.929388278055756,
-	#                 0.6590640584585284,
-	#                 0.2945402188991307,
-	#                 0.058067133025310004,
-	#                 0.12105634855567009,
-	#                 0.03824844114583343,
-	#                 0.08602808574207428,
-	#                 0.2931304818815008,
-	#                 0.0261180827517812,
-	#                 0.03101563541907137,
-	#                 0.013883681157457802,
-	#                 0.0461090926649544,
-	#                 0.009065208892478673,
-	#                 0.0109488022551917],
-	#                -4.252044807925374,
-	#                -4.126526619249349,
-	#                -1.5392693567147941)
-	
 	final_axis_y = final_result[0]
 	final_axis_x = [n for n in range(3, (len(final_axis_y)+3))]
-	ic(final_axis_y)
-	ic(final_axis_x)
 
 	sc = MplCanvas(width=5, height=4, dpi=100)
 	sc.axes.plot(final_axis_x, final_axis_y)
@@ -522,10 +508,10 @@ def stop_record_data():
 	print("Data recording stopped")
 
 class MplCanvas(FigureCanvasQTAgg):
-	def __init__(self, parent=None, width=5, height=4, dpi=100):
-		fig = Figure(figsize=(width, height), dpi=dpi)
-		self.axes = fig.add_subplot(111)
-		super(MplCanvas, self).__init__(fig)
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 class window(QtWidgets.QMainWindow):
 	def __init__(self):
