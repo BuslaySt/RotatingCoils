630dd4f5c5a75c8bca8b28c938719b4f588eeedd
Stepan Martyushov <techseth@yandex.ru>
2024-06-05 16:42:21 +0300

    Update result_show.py

--- result_show.py
+++ result_show.py
@@ -23,13 +23,12 @@ class PandasTableModel(QAbstractTableModel):
     def data(self, index, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
             value = self.data.iloc[index.row(), index.column()]
-            # if np.isnan(value):
+
             if pd.isnull(value):
                 return '-'
 
             if isinstance(value, float):
-                # Render float to 6 dp
-                return f"{value:.6f}"
+                return f'{value:.3g}'
 
         return None
 
@@ -42,13 +41,6 @@ class PandasTableModel(QAbstractTableModel):
             if orientation == Qt.Vertical:
                 return self.vheaders[section]
 
-    # def headerData(self, section, orientation, role=Qt.DisplayRole):
-    #     if role == Qt.DisplayRole and orientation == Qt.Horizontal:
-    #         return self.hheaders[section]
-    #     if role == Qt.DisplayRole and orientation == Qt.Vertical:
-    #         return self.vheaders[section]
-    #     return None
-
 class MainUI(QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
@@ -56,16 +48,17 @@ class MainUI(QMainWindow):
         self.setWindowIcon(QIcon("logo.png"))
         self.tabWidget.setTabEnabled(1, True)
 
-        df = pd.read_csv("result.csv")
-        df.rename(columns = {'Unnamed: 0':'name'}, inplace = True)
-        df["mean"] = df.iloc[:, 1:5].mean(axis=1)
-        df["stdev"] = df.iloc[:, 1:5].std(axis=1)
-        df["percent"] = (100*df.iloc[:, 1:5].std(axis=1)/df.iloc[:, 1:5].mean(axis=1)).round()
-        df.drop(['name', '0', '1', '2', '3', '4'], axis=1, inplace=True)
-        # print(df)
-        df_header_h = ['Среднее', 'Абс. откл.', 'Откл. в %']
-        df_header_v = ["harm01", "harm02", "harm03", "harm04", "harm05", "harm06", "harm07", "harm08", "harm09", "harm10",
-                        "harm11", "harm12", "harm13", "harm14", "harm15", "harm16", "deltaX", "deltaY", "alpha", 'H_avg']
+        df = pd.read_csv("result.csv", index_col=0)
+        
+        print(df)
+
+        # df['mean'] = df.mean(axis=1)
+        # df['stdev'] = df.drop(['mean'], axis=1).std(axis=1)
+        # df["percent"] = (100*df["stdev"]/df["mean"]).round(decimals=1).abs()
+
+        df_header_v = df.index
+        df_header_h = df.columns
+
         model = PandasTableModel(df, df_header_h, df_header_v)
         self.tblView_Result_1.setModel(model)
    
