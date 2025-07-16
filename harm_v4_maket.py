9b153e370e466ca4a5cd2f7250920251a705ae3a
Stepan Martyushov <techseth@yandex.ru>
2025-07-16 16:54:32 +0300

    Update harm_v4_maket.py
    self.cBox_SaveRawData.isChecked()

--- harm_v4_maket.py
+++ harm_v4_maket.py
@@ -689,8 +689,9 @@ class MainUI( QMainWindow):
         df = pd.DataFrame(self.data)
         df['D0'] = df['D0'].apply(int)
         df['D1'] = df['D1'].apply(int)
-
-        #self.save_data2file(df)
+        
+        if self.cBox_SaveRawData.isChecked():
+            self.save_data2file(df)
         return df
 
     def save_data2file(self, df: pd.DataFrame, i: int = '') -> None:
